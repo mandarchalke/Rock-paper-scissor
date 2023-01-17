f2c35b8e81fa94224e5bb3c9efe28f1e99afd32c
chalkemandar71 <chalkemandar71@gmail.com>
2023-01-17 11:35:25 +0530

    Game function created.
    *This game function compares player input and computer input and displays resutl.

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-let a = Math.floor(Math.random()*3);
+const a = Math.floor(Math.random() * 3);
 
-function getComputerchoice(a) {
+function getComputerChoice(a) {
     if (a === 0) {
         return "Rock";
     } else if (a === 1) {
-       return "Paper"; 
-    } else if(a === 2) {
+        return "Paper";
+    } else if (a === 2) {
         return "Scissors";
-    } 
+    }
 }
-console.log(getComputerchoice(a));
 
+const computerSelection = getComputerChoice(a);
+const playerSelection = prompt("Enter your choice: ",);
+
+
+function game(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        console.log("You lose!");
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        console.log("You win!");
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        console.log("You win!");
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        console.log("You Lose!");
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        console.log("You win!");
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        console.log("You lose!");
+    }
+}
+
+game(playerSelection, computerSelection);
